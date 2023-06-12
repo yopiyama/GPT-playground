e84b7e2373e60131762aa4df8a057deccf709dd9
yopiyama <yoshiyama.r24@gmail.com>
2023-06-12 12:42:14 +0000

    [Update] Model リスト確認用のコマンドを微修正

--- verification/avail_model_list.sh
+++ verification/avail_model_list.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl https://api.openai.com/v1/models -H "Authorization: Bearer $OPENAI_API_KEY" | jq .data[].id
+curl -s https://api.openai.com/v1/models -H "Authorization: Bearer $OPENAI_API_KEY" | jq .data[].id | sort | jq
